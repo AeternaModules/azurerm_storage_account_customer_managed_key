7f2d42f03ab3a7192bb5e532ccbd8a86dfc3249e
Daniel Allen <dan@aeternagroup.net>
2026-07-29 23:40:39 +0100

    Update to schema version v5.0.0

--- outputs.tf
+++ outputs.tf
@@ -6,29 +6,9 @@ output "storage_account_customer_managed_keys_federated_identity_client_id" {
   description = "Map of federated_identity_client_id values across all storage_account_customer_managed_keys, keyed the same as var.storage_account_customer_managed_keys"
   value       = { for k, v in azurerm_storage_account_customer_managed_key.storage_account_customer_managed_keys : k => v.federated_identity_client_id if v.federated_identity_client_id != null && length(v.federated_identity_client_id) > 0 }
 }
-output "storage_account_customer_managed_keys_key_name" {
-  description = "Map of key_name values across all storage_account_customer_managed_keys, keyed the same as var.storage_account_customer_managed_keys"
-  value       = { for k, v in azurerm_storage_account_customer_managed_key.storage_account_customer_managed_keys : k => v.key_name if v.key_name != null && length(v.key_name) > 0 }
-}
-output "storage_account_customer_managed_keys_key_vault_id" {
-  description = "Map of key_vault_id values across all storage_account_customer_managed_keys, keyed the same as var.storage_account_customer_managed_keys"
-  value       = { for k, v in azurerm_storage_account_customer_managed_key.storage_account_customer_managed_keys : k => v.key_vault_id if v.key_vault_id != null && length(v.key_vault_id) > 0 }
-}
 output "storage_account_customer_managed_keys_key_vault_key_id" {
   description = "Map of key_vault_key_id values across all storage_account_customer_managed_keys, keyed the same as var.storage_account_customer_managed_keys"
   value       = { for k, v in azurerm_storage_account_customer_managed_key.storage_account_customer_managed_keys : k => v.key_vault_key_id if v.key_vault_key_id != null && length(v.key_vault_key_id) > 0 }
-}
-output "storage_account_customer_managed_keys_key_vault_uri" {
-  description = "Map of key_vault_uri values across all storage_account_customer_managed_keys, keyed the same as var.storage_account_customer_managed_keys"
-  value       = { for k, v in azurerm_storage_account_customer_managed_key.storage_account_customer_managed_keys : k => v.key_vault_uri if v.key_vault_uri != null && length(v.key_vault_uri) > 0 }
-}
-output "storage_account_customer_managed_keys_key_version" {
-  description = "Map of key_version values across all storage_account_customer_managed_keys, keyed the same as var.storage_account_customer_managed_keys"
-  value       = { for k, v in azurerm_storage_account_customer_managed_key.storage_account_customer_managed_keys : k => v.key_version if v.key_version != null && length(v.key_version) > 0 }
-}
-output "storage_account_customer_managed_keys_managed_hsm_key_id" {
-  description = "Map of managed_hsm_key_id values across all storage_account_customer_managed_keys, keyed the same as var.storage_account_customer_managed_keys"
-  value       = { for k, v in azurerm_storage_account_customer_managed_key.storage_account_customer_managed_keys : k => v.managed_hsm_key_id if v.managed_hsm_key_id != null && length(v.managed_hsm_key_id) > 0 }
 }
 output "storage_account_customer_managed_keys_storage_account_id" {
   description = "Map of storage_account_id values across all storage_account_customer_managed_keys, keyed the same as var.storage_account_customer_managed_keys"
--- variables.tf
+++ variables.tf
@@ -2,27 +2,17 @@ variable "storage_account_customer_managed_keys" {
   description = <<EOT
 Map of storage_account_customer_managed_keys, attributes below
 Required:
+    - key_vault_key_id
     - storage_account_id
 Optional:
     - federated_identity_client_id
-    - key_name
-    - key_vault_id
-    - key_vault_key_id
-    - key_vault_uri
-    - key_version
-    - managed_hsm_key_id
     - user_assigned_identity_id
 EOT
 
   type = map(object({
+    key_vault_key_id             = string
     storage_account_id           = string
     federated_identity_client_id = optional(string)
-    key_name                     = optional(string)
-    key_vault_id                 = optional(string)
-    key_vault_key_id             = optional(string)
-    key_vault_uri                = optional(string)
-    key_version                  = optional(string)
-    managed_hsm_key_id           = optional(string)
     user_assigned_identity_id    = optional(string)
   }))
   validation {
